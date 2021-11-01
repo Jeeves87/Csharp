cdf8f08640f5710095931ebfc869f6a88316a14c
Jeeves87 <reevesj812@gmail.com>
2021-11-01 06:52:21 -0500

    Complete refactoring. Adds State class, moves almost all functionality over to it. Improves main program logic.

---
+++ AreaCodeReader/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AreaCodeReader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            string[] csvLines = System.IO.File.ReadAllLines("AreaCodeRaw.csv");
+            var country = new List<State>();          // Creates a list country of the type State
+
+            for (int i = 0; i < csvLines.Length; i++) // For every line in the csv do:
+            {
+                State state = new State(csvLines[i]);// Make a new instance of State named state                
+                country.Add(state);                   // Add state to list country                                               
+            }
+            
+
+            while (true)
+            {
+                Console.WriteLine("Enter a US State or territory to receive its associated area code(s).");
+                Console.WriteLine("Or enter a valid US area code to find out its associate state");
+                Console.WriteLine("Or 'quit' to terminate the program.");
+                string UserNum = Console.ReadLine();
+
+                for (int z = 0; z < country.Count; z++)  // Loops as many times as there are entries into country 
+                {                                        // I think this is where I need to add a third loop 
+                    if (country[z].Any(UserNum) == true) // UserNum is definitely a part of some state in country
+                    {
+                        if (country[z].Name(UserNum) == true)
+                        {
+                            Console.WriteLine($"{UserNum}'s area code(s) are: {country[z].AreaCode1}, {country[z].AreaCode2}, {country[z].AreaCode3}, {country[z].AreaCode4}, {country[z].AreaCode5}, {country[z].AreaCode6}, {country[z].AreaCode7}, {country[z].AreaCode8}, {country[z].AreaCode9}, {country[z].AreaCode10}, {country[z].AreaCode11}, {country[z].AreaCode12}, {country[z].AreaCode13}, {country[z].AreaCode14}, {country[z].AreaCode15}, {country[z].AreaCode16}, {country[z].AreaCode17}, {country[z].AreaCode18}, {country[z].AreaCode19}, {country[z].AreaCode20}, {country[z].AreaCode21}, {country[z].AreaCode22}, {country[z].AreaCode23}, {country[z].AreaCode24}, {country[z].AreaCode25}, {country[z].AreaCode26}, {country[z].AreaCode27}, {country[z].AreaCode28}");
+                        };
+                        if (country[z].AreaCode(UserNum) == true)
+                        {
+                            Console.WriteLine($"Your area code, {UserNum}, is associated with {country[z].StateName}");
+                        }
+
+
+                    }
+
+                }
+                if (UserNum == "quit")
+                {
+                    return;
+                }
+            }
+
+
+        }
+
+    }
+    
+}
---
+++ AreaCodeReader/State.cs
@@ -0,0 +1,92 @@
+﻿using System;
+
+namespace AreaCodeReader
+{
+    public class State
+    {
+        public string StateName, AreaCode1, AreaCode2, AreaCode3, AreaCode4, AreaCode5, AreaCode6, AreaCode7, AreaCode8, AreaCode9, AreaCode10, AreaCode11, AreaCode12, AreaCode13, AreaCode14, AreaCode15, AreaCode16, AreaCode17, AreaCode18, AreaCode19, AreaCode20, AreaCode21, AreaCode22, AreaCode23, AreaCode24, AreaCode25, AreaCode26, AreaCode27, AreaCode28;
+        
+        
+        
+        public State(string rowData)
+        {
+            string[] data = rowData.Split(',');
+            
+            this.StateName = data[0];
+            this.AreaCode1 = data[1];
+            this.AreaCode2 = data[2];
+            this.AreaCode3 = data[3];
+            this.AreaCode4 = data[4];
+            this.AreaCode5 = data[5];
+            this.AreaCode6 = data[6];
+            this.AreaCode7 = data[7];
+            this.AreaCode8 = data[8];
+            this.AreaCode9 = data[9];
+            this.AreaCode10 = data[10];
+            this.AreaCode11 = data[11];
+            this.AreaCode12 = data[12];
+            this.AreaCode13 = data[13];
+            this.AreaCode14 = data[14];
+            this.AreaCode15 = data[15];
+            this.AreaCode16 = data[16];
+            this.AreaCode17 = data[17];
+            this.AreaCode18 = data[18];
+            this.AreaCode19 = data[19];
+            this.AreaCode20 = data[20];
+            this.AreaCode21 = data[21];
+            this.AreaCode22 = data[22];
+            this.AreaCode23 = data[23];
+            this.AreaCode24 = data[24];
+            this.AreaCode25 = data[25];
+            this.AreaCode26 = data[26];
+            this.AreaCode27 = data[27];
+            this.AreaCode28 = data[28];
+        }
+
+               
+        public override string ToString()
+        {
+            string Full = $"Your area code is associated with {StateName}";
+            return Full;
+        }
+
+        public bool Any(string value)
+        {
+            
+            if (StateName == value || AreaCode1 == value || AreaCode2 == value || AreaCode3 == value || AreaCode4 == value || AreaCode5 == value || AreaCode6 == value || AreaCode7 == value || AreaCode8 == value || AreaCode9 == value || AreaCode10 == value || AreaCode11 == value || AreaCode12 == value || AreaCode13 == value || AreaCode14 == value || AreaCode15 == value || AreaCode16 == value || AreaCode17 == value || AreaCode18 == value || AreaCode19 == value || AreaCode20 == value || AreaCode21 == value || AreaCode22 == value || AreaCode23 == value || AreaCode24 == value || AreaCode25 == value || AreaCode26 == value || AreaCode27 == value || AreaCode28 == value)
+            { 
+                return true;
+            } else
+            {
+                return false;
+            }
+            
+        }
+
+        public bool Name(string value)
+        {
+            if (StateName == value)
+            {
+                return true;
+            } else
+            {
+                return false;
+            }
+        }
+
+        public bool AreaCode(string value)
+        {
+            if (AreaCode1 == value || AreaCode2 == value || AreaCode3 == value || AreaCode4 == value || AreaCode5 == value || AreaCode6 == value || AreaCode7 == value || AreaCode8 == value || AreaCode9 == value || AreaCode10 == value || AreaCode11 == value || AreaCode12 == value || AreaCode13 == value || AreaCode14 == value || AreaCode15 == value || AreaCode16 == value || AreaCode17 == value || AreaCode18 == value || AreaCode19 == value || AreaCode20 == value || AreaCode21 == value || AreaCode22 == value || AreaCode23 == value || AreaCode24 == value || AreaCode25 == value || AreaCode26 == value || AreaCode27 == value || AreaCode28 == value)
+            {
+                return true;
+            } else
+            {
+                return false;
+            }
+        }
+    }
+    
+    
+}
+
+    
